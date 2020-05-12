f1b74dd06d83c694697a2f78cee2afc06431f575
tpurnell00 <65251458+tpurnell00@users.noreply.github.com>
2020-05-12 14:53:54 -0500

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'awk_fu+8g)xgk(x*p$h&bvo^ftu(5gn54guhp7%&r7w%r+$#kj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1',]
+ALLOWED_HOSTS = ['127.0.0.1','timpurnell.pythonanywhere.com',]
 
 
 # Application definition
